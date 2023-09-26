900651ea4e540703ba754e7d53c0e1bc7ec5c102
redsleo02 <127219441+redsleo02@users.noreply.github.com>
2023-09-26 20:40:32 +0200

    Updated map and reduce functions

--- Tasks/app/src/main/java/io/grpc/filesystem/task2/MapReduce.java
+++ Tasks/app/src/main/java/io/grpc/filesystem/task2/MapReduce.java
@@ -56,15 +56,25 @@ public class MapReduce {
      */
     public static void map(String inputfilepath) throws IOException {
 
-        /*
-         * Insert your code here
-         * Take a chunk and filter words (you could use "\\p{Punct}" for filtering punctuations and "^[a-zA-Z0-9]"
-         * together for filtering the words), then split the sentences to take out words and assign "1" as the initial count.
-         * Use the given mapper class to create the unsorted key-value pair.
-         * Save the map output in a file named "map-chunk001", for example, in folder
-         * path input/temp/map
-         */
+        try (BufferedReader br = new BufferedReader(new FileReader(inputFilePath))) {
+        String line;
+        int chunkCount = 1;
 
+        while ((line = br.readLine()) != null) {
+            String[] words = line.split("\\s+");
+            for (String word : words) {
+                // Filter out punctuations and non-alphanumeric characters
+                word = word.replaceAll("\\p{Punct}", "");
+                word = word.replaceAll("^[^a-zA-Z0-9]", "");
+
+                // Check if the word is not empty after filtering
+                if (!word.isEmpty()) {
+                    // Create a key-value pair with word and initial count 1
+                    Mapper.emitIntermediate(word, "1");
+                }
+            }
+        }
+    }
     }
 
     /**
@@ -74,14 +84,44 @@ public class MapReduce {
      * @throws IOException
      */
     public static void reduce(String inputfilepath, String outputfilepath) throws IOException {
+    // Create a HashMap to store word counts
+        Map<String, Integer> wordCounts = new HashMap<>();
 
-        /*
-         * Insert your code here
-         * Take all the files in the map folder and reduce them to one file that shows
-         * unique words with their counts as "the:64", for example.
-         * Save the output of reduce function as output-task2.txt
-         */
+        // Get a list of all files in the map folder
+        File mapFolder = new File(inputFolderPath + "/map");
+        File[] mapFiles = mapFolder.listFiles();
 
+        if (mapFiles != null) {
+            // Iterate through each map file
+            for (File mapFile : mapFiles) {
+                try (BufferedReader br = new BufferedReader(new FileReader(mapFile))) {
+                    String line;
+
+                    // Read each line and split it into key-value pairs
+                    while ((line = br.readLine()) != null) {
+                        String[] keyValue = line.split(":");
+                        if (keyValue.length == 2) {
+                            String word = keyValue[0].trim();
+                            int count = Integer.parseInt(keyValue[1].trim());
+
+                            // Update word counts in the HashMap
+                            wordCounts.put(word, wordCounts.getOrDefault(word, 0) + count);
+                        }
+                    }
+                }
+            }
+
+            // Write the unique words and their counts to the output file
+            try (BufferedWriter bw = new BufferedWriter(new FileWriter(outputFilePath))) {
+                for (Map.Entry<String, Integer> entry : wordCounts.entrySet()) {
+                    String word = entry.getKey();
+                    int count = entry.getValue();
+                    String outputLine = word + ":" + count;
+                    bw.write(outputLine);
+                    bw.newLine();
+                }
+            }
+        }
     }
 
     /**
